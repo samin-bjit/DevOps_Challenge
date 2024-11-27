54ab599306b890286e6ac4cf9386e78ef5f8d5f3
Fahimul Hoque <fahimulhoquejason@gmail.com>
2024-11-27 13:49:18 +0000

    Fix DB user issue for backend

--- mysql/init-db.sh
+++ mysql/init-db.sh
@@ -4,6 +4,7 @@ echo "Creating new user and granting privileges..."
 
 mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    ALTER USER '${MYSQL_USER}' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;
     FLUSH PRIVILEGES;
 "
